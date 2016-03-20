994c419c401d4afc39da640d67ab66f1be4ba85a
Sendy Halim <sendyhalim93@gmail.com>
2016-03-20 13:11:49 +0700

    Add PushButtonView

--- CoreGraphic/Flo/Flo/PushButtonView.swift
+++ CoreGraphic/Flo/Flo/PushButtonView.swift
@@ -8,10 +8,57 @@
 
 import UIKit
 
+@IBDesignable
 class PushButtonView: UIButton {
+  var isAddButton = true
+
   override func drawRect(rect: CGRect) {
     let path = UIBezierPath(ovalInRect: rect)
-    UIColor.greenColor().setFill()
+    UIColor.blueColor().setFill()
     path.fill()
+    drawPlus()
+  }
+
+  func drawPlus() {
+    let plusHeight: CGFloat = 3.0
+    let plusWidth: CGFloat = min(bounds.width, bounds.height) * 0.4
+    let plusPath = UIBezierPath()
+    let middleX = bounds.width / 2
+    let middleY = bounds.height / 2
+
+    plusPath.lineWidth = plusHeight
+
+    /// If you have oddly sized straight lines, you’ll need to position them at
+    /// plus or minus 0.5 points to prevent anti-aliasing.
+    /// If you look at the diagrams above, you’ll see that a half point on
+    /// the iPad 2 will move the line up half a pixel, on the iPhone 6,
+    /// up one whole pixel, and on the iPhone 6 Plus, up one and a half pixels.
+
+    /// Draw the horizontal line
+    plusPath.moveToPoint(CGPoint(
+      x: middleX - (plusWidth / 2) + 0.5,
+      y: middleY + 0.5
+    ))
+
+    plusPath.addLineToPoint(CGPoint(
+      x: middleX + plusWidth / 2 + 0.5,
+      y: middleY + 0.5
+    ))
+
+    if isAddButton {
+      ///  Draw the vertical line
+      plusPath.moveToPoint(CGPoint(
+        x: middleX + 0.5,
+        y: middleY - (plusWidth / 2) + 0.5
+      ))
+
+      plusPath.addLineToPoint(CGPoint(
+        x: middleX + 0.5,
+        y: middleY + (plusWidth / 2) + 0.5
+      ))
+    }
+
+    UIColor.whiteColor().setStroke()
+    plusPath.stroke()
   }
 }
