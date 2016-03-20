2ee8b19ff7e5f72e1fa88b12ab885047bb305f38
Sendy Halim <sendyhalim93@gmail.com>
2016-03-20 12:10:50 +0700

    Add animate method

--- Gradient/Gradient Background/Gradient Background/ViewController.swift
+++ Gradient/Gradient Background/Gradient Background/ViewController.swift
@@ -12,6 +12,7 @@ import Chameleon
 /// Reference http://blog.apoorvmote.com/gradient-background-uiview-ios-9-swift
 class ViewController: UIViewController {
   let gradientLayer = CAGradientLayer()
+
   override func viewDidLoad() {
     super.viewDidLoad()
 
@@ -21,9 +22,34 @@ class ViewController: UIViewController {
     let color1 = UIColor.flatSkyBlueColor().CGColor as CGColorRef
     let color2 = UIColor.flatMintColor().CGColor as CGColorRef
     let color3 = UIColor.flatMagentaColor().CGColor as CGColorRef
-    let color4 = UIColor.flatRedColor().CGColor as CGColorRef
-    gradientLayer.colors = [color1, color2, color3, color4]
+    gradientLayer.colors = [color1, color2, color3]
 
     view.layer.addSublayer(gradientLayer)
+    animate()
+  }
+
+  func animate() {
+    let fromColors = gradientLayer.colors
+    let toColors = [
+      UIColor.flatMintColor().CGColor as CGColorRef,
+      UIColor.flatMagentaColor().CGColor as CGColorRef,
+      UIColor.flatSkyBlueColor().CGColor as CGColorRef,
+    ]
+
+    let animation: CABasicAnimation = CABasicAnimation(keyPath: "colors")
+    animation.fromValue = fromColors
+    animation.toValue = toColors
+    animation.duration = 3.00
+    animation.removedOnCompletion = false
+    animation.autoreverses = true
+    animation.fillMode = kCAFillModeForwards
+    animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+    animation.repeatCount = Float.infinity
+    animation.delegate = self
+    gradientLayer.addAnimation(animation, forKey: "animateGradient")
+
+    // let radiansToRotate = CGFloat(90 * M_PI / 180)
+    // let transform = CATransform3DMakeRotation(radiansToRotate, 0, 0, 1.0)
+    // gradientLayer.transform = transform
   }
 }
